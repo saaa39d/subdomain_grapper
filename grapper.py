af5d986ae1855302f06bb4d108bb714cabc0b9cf
c!tchmyIfyoucn <31493122+saaa39d@users.noreply.github.com>
2020-12-06 04:13:45 +0300

    Add files via upload
    after extract subdomains from any tool for example in my case sublist3r
    all u have to do save it in text file but remember  without http or https or ://
    For example
    desk.site.com
    Or
    dev.site.com

---
+++ grapper.py
@@ -0,0 +1,21 @@
+import requests,concurrent.futures,time,sys
+print('usage: python3 grapper.py subs.txt #note subs.txt the domains without http,s or :// \n for ex: subdomain.site.com \t or help.google.com ')
+print("\n")
+def speed():
+	_file=sys.argv[1]
+	open_file= open(_file ,"r").read().splitlines()
+	for i in open_file:
+		try:
+			url=requests.get("https://"+i)
+			print(url.url,"\t","status_code is: ",url.status_code)
+		except:
+			print("http not work i will try https")
+			try:
+				url1=requests.get("http://"+i)
+				print(url.url,"\t","status_code is: ",url.status_code)
+			except:
+				print("bad url" ,i)
+
+with concurrent.futures.ThreadPoolExecutor() as pl:
+	time.sleep(0.2)
+	pl.submit(speed)
